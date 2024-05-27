c3a6a1fa9d838bc0d3cc9e3bf4b4a78535e217ef
nparask <150552008+nparask@users.noreply.github.com>
2024-05-27 18:50:20 +0000

    modified:   app/main.py     added tracking player stats
    	new file:   app/stats.txt
        where the player stats are stored

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 from random import randint
 
-# Game Logic
+#Init vars
 playing = True
 attempts = 1
+guesses = ""
 pick = randint(1, 100)
 
+# Game Logic
 def logic(guess):
     global playing, attempts
     if guess == pick:
         playing = False
         flash(f"You found it! The number was {pick}")
+        save_stats()
         if attempts == 1:
             flash(f"It took you {attempts} attempt to find it")
         else:
@@ -30,18 +33,27 @@ def reset():
     pick = randint(1, 100)
     attempts = 1
 
+def save_stats():
+    stats_file = open("app/stats.txt", "a")
+    stats_file.write(f"The number was {pick}.\tNumber of attempts was: {attempts}\tThe guesses the user made were: {guesses}\n")
+    stats_file.close()
+
 # Flask init
 app = Flask(__name__)
 app.secret_key = "iwrfjoiwfioweflp"
 
 @app.route('/', methods=["POST", "GET"])
 def main():
+    global guesses
     message = None
 
     ###########ChatGPT#########
     if request.method == "POST":
         if 'input' in request.form:
-            guess = int(request.form["input"])
+            guess = request.form["input"]
+            guesses = guesses + guess + ', '
+            guess = int(guess)
+
             message = logic(guess)
 
         elif 'reset' in request.form:
